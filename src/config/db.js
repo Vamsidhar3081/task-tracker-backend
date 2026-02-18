dfe970776075997141412db7369bbcdfe941f2a7
vamsi-intern-synclovis <vamsi.intern.synclovis@gmail.com>
2026-02-18 12:01:58 +0530

    deploy

--- src/config/db.js
+++ src/config/db.js
@@ -4,9 +4,9 @@ dotenv.config();
 import mysql from "mysql2/promise";
 
 export const db = mysql.createPool({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASS,
-  database: process.env.DB_NAME
+  host: process.env.MYSQLHOST,
+  user: process.env.MYSQLUSER,
+  password: process.env.MYSQLPASSWORD,
+  database: process.env.MYSQLDATABASE
 });
 export default db;
--- src/server.js
+++ src/server.js
@@ -4,7 +4,7 @@ dotenv.config();
 import app from "./app.js";
 import { db } from "./config/db.js";
 
-const PORT = process.env.PORT||5000;
+const PORT = process.env.MYSQLPORT;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
