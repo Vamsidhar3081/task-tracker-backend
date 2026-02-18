3fe148d734a6d29dcc30a92ea4d16cb692e82fa3
vamsi-intern-synclovis <vamsi.intern.synclovis@gmail.com>
2026-02-18 11:24:20 +0530

    deploy

--- src/server.js
+++ src/server.js
@@ -4,7 +4,7 @@ dotenv.config();
 import app from "./app.js";
 import { db } from "./config/db.js";
 
-const PORT = process.env.PORT;
+const PORT = process.env.PORT||5000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
