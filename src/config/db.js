3b15cd863d0bbb06a1145d0c06ff0ed128918f66
vamsi-intern-synclovis <vamsi.intern.synclovis@gmail.com>
2026-04-16 15:38:25 +0530

    backend db hosted

--- src/config/db.js
+++ src/config/db.js
@@ -1,12 +1,21 @@
-import dotenv from 'dotenv';
+import dotenv from "dotenv";
 dotenv.config();
 
 import mysql from "mysql2/promise";
 
 export const db = mysql.createPool({
   host: process.env.MYSQLHOST,
+  port: process.env.MYSQLPORT,
   user: process.env.MYSQLUSER,
   password: process.env.MYSQLPASSWORD,
-  database: process.env.MYSQLDATABASE
+  database: process.env.MYSQLDATABASE,
+
+  ssl: {
+    rejectUnauthorized: false,
+  },
+
+  waitForConnections: true,
+  connectionLimit: 10,
 });
+
 export default db;
